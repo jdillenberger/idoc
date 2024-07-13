bfabd30d103e653a3bb1e771dffe2957a805ce4a
Jan Dillenberger <jdillenberger@users.noreply.github.com>
2024-07-14 01:38:47 +0200

    Fix type error with array type

--- resources/views/languages/bash.blade.php
+++ resources/views/languages/bash.blade.php
@@ -6,6 +6,13 @@ curl -X {{$route['methods'][0]}} {{$route['methods'][0] == 'GET' ? '-G ' : ''}}"
 @endif
 
 @foreach($route['bodyParameters'] as $attribute => $parameter)
+    @if(gettype($parameter['value']) === "boolean")\
+    -d "{{ $attribute}}={!! "false" !!}"
+    @elseif (in_array(gettype($parameter['value']), ["array", "object"])\
+    -d "{{ $attribute}}={!! json_encode($parameter['value']) !!}"
+    @else\
     -d "{{$attribute}}"="{!!$parameter['value'] === false ? "false" : $parameter['value']!!}" @if(! ($loop->last))\
-@endif
+    @endif
+    @endif
+@endforeach
 @endforeach
